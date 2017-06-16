debbafb5087c54259f8129c6202be4fd8e2226f2
AngelicaMVG <angie.velazquez2211@gmail.com>
2017-06-16 12:56:21 -0500

    filtro de busqueda

--- js/main.js
+++ js/main.js
@@ -3,36 +3,39 @@ var api = {
 };
 
 var plantillaFinal = '';
-
+var arregloTemas = [];
 
 var cargarPagina = function() {
   cargarTemas();
   $('#add-form').submit(agregarTema);
-  // $("#search-form").submit(filtrarTema);
+  $("#search-form").submit(filtrarTema);
 };
 
 var cargarTemas = function() {
   $.getJSON(api.url, function(topics) {
-    topics.forEach(function(tema) {
-      var autor = tema.author_name;
-      var contenido = tema.content;
-      var id = tema.id;
-      var respuestas = tema.responses_count;
-      plantillaFinal += plantilla.replace("__autor__", autor)
-        .replace("__contenido__", contenido)
-        .replace("__id__", id)
-        .replace("__respuestas__", respuestas);
-    });
-    $('#temas').html(plantillaFinal);
+    arregloTemas = topics;
+    topics.forEach(mostrarTema);
   });
+};
+
+var mostrarTema = function(tema) {
+  var autor = tema.author_name;
+  var contenido = tema.content;
+  var id = tema.id;
+  var respuestas = tema.responses_count;
+  plantillaFinal = plantilla.replace("__autor__", autor)
+    .replace("__contenido__", contenido)
+    .replace("__id__", id)
+    .replace("__respuestas__", respuestas);
+    $('#temas').prepend(plantillaFinal);
 };
 
 
 var plantilla= '<div class="jumbotron">' +
   '<h2>__contenido__</h2>' +
-  '<p>__autor__</p>' +
-  '<p>__respuestas__</p>' +
-  '<a href="verTopic.html?topic_id=__id__">+</a>'+
+  '<p>Por: __autor__</p>' +
+  '<a href="verTopic.html?topic_id=__id__"><i class="glyphicon glyphicon-plus"></i> Ver mas</a>'+
+  '<span class="num-res">Respuestas: __respuestas__</span>' +
   '</div>';
 
 
@@ -50,23 +53,17 @@ var agregarTema = function(e) {
 };
 
 
-// var filtrarTema = function(e) {
-//   e.preventDefault();
-//   var criterioBusqueda = $("#search").val().toLowerCase();
-//   var id = plantillaFinal.id;
-//   var autor = plantillaFinal.autor;
-//   console.log(criterioBusqueda);
-//   $.getJSON(api.url, {
-//     content: criterioBusqueda,
-//     id: id,
-//     author_name: autor
-//   }, function(response){
-//       var temaFiltrado = response.filter(function(plantillaFinal){
-//       return plantillaFinal.content.toLowerCase().indexOf(criterioBusqueda) >= 0;
-//     });
-//     crearTema(temaFiltrado);
-//   });
-// };
+
+var filtrarTema = function(e) {
+  e.preventDefault();
+  var criterioBusqueda = $("#search").val().toLowerCase();
+  console.log(criterioBusqueda);
+  var temasFiltrados = arregloTemas.filter(function(tema) {
+    return tema.content.toLowerCase().indexOf(criterioBusqueda) >= 0;
+  });
+  $('#temas').html('');
+  temasFiltrados.forEach(mostrarTema);
+};
 
 
 
--- js/verTopic.js
+++ js/verTopic.js
@@ -1,9 +1,7 @@
 var topicId = getParameterByName('topic_id');
 
 //Solo por propositos de debug
-if(topicId){
-  alert("El topic ID es:"+topicId);
-}
+
 
 var api = {
   url: 'http://examen-laboratoria-sprint-5.herokuapp.com/topics/'+ topicId
@@ -11,33 +9,49 @@ var api = {
 var plantillaFinal = '';
 var cargarPagina = function() {
   cargarTemas();
+  $('#add-form').submit(agregarTema);
 };
 
 var cargarTemas = function() {
   $.getJSON(api.url, function(tema) {
-
       var autor = tema.author_name;
       var contenido = tema.content;
       var id = tema.id;
+      var fecha = tema.created_at;
       var respuestas = tema.responses_count;
-      plantillaFinal += plantilla.replace("__autor__", autor)
+      plantillaFinal = plantilla.replace("__autor__", autor)
         .replace("__contenido__", contenido)
         .replace("__id__", id)
-        .replace("__respuestas__", respuestas);
+        .replace("__creado__", fecha);
 
-    $('#topic').html(plantillaFinal);
+    $('#topic').append(plantillaFinal);
   });
 
 };
 
 
 var plantilla= '<div class="jumbotron">' +
+  '<div class= "contenido-res">' +
   '<h2>__contenido__</h2>' +
   '<p>__autor__</p>' +
-  '<p>__respuestas__</p>' +
-  '<button class="btn-success btn-sm" data-toggle="modal" data-target="#myModal">Respuesta</button>'+
+  '<span><small> Creado: __creado__ </small></span>' +
+  '<button class="btn-success btn-sm btn-res" data-toggle="modal" data-target="#myModal">Respuesta</button>'+
+  '</div>' +
   '</div>';
 
+
+  var agregarTema = function(e) {
+    e.preventDefault();
+    var autor = $('#autor-res').val();
+    var contenido = $('#contenido-res').val();
+    $.post(api.url, {
+      author_name: autor,
+      content: contenido
+    }, function(response){
+      $('#myModal').modal('hide');
+      cargarTemas();
+    });
+  };
 
 
   $(document).ready(cargarPagina);
